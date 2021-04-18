4db50f6e6f4f6233dd57baf7e06602a7aaafad01
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-04-18 19:15:03 +0900

    210418

---
+++ 콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    var answer = 0;
+    var temp = num;
+    while(temp!==1) {
+        if(temp%2===0) {
+            temp = temp/2;
+        } else {
+            temp = temp*3+1;
+        }
+        answer++;
+        if(answer===500) {
+            answer = -1;
+            break;
+        }
+    }
+    return answer;
+}
